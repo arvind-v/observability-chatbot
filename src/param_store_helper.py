4b99591dd6c201def263c14fc4f1387a4cf55d8b
Steven David <stevendavid@outlook.com>
2023-11-17 18:29:11 -0800

    Synced updates from steven local param_store_helper.py

--- src/param_store_helper.py
+++ src/param_store_helper.py
@@ -6,7 +6,7 @@ class ParameterStoreHelper:
   # in SSM parameter store
   def __init__ (self, basePath):
     self.ssm = boto3.client('ssm')
-    self.basePath = basePath
+    #self.basePath = basePath
     self.host = self.getParameter('host')
     self.user = self.getParameter('user', decrypt=True)
     self.password = self.getParameter('password', decrypt=True)
@@ -14,7 +14,7 @@ class ParameterStoreHelper:
   # Get parameter from SSM parameter store
   def getParameter (self, parameterName, decrypt=False):
     return self.ssm.get_parameter(
-      Name=self.basePath + '/' + parameterName,
+      Name=parameterName,
       WithDecryption=decrypt
     )['Parameter']['Value']
   
@@ -29,4 +29,3 @@ class ParameterStoreHelper:
   # Get password
   def getPassword (self):
     return self.getParameter('password', decrypt=True)
-  
